ffe6c4c022e34848c4489fcc71a2080efbc183ae
zhaobod1 <zhaobod1@163.com>
2017-03-17 13:43:28 +0800

    add examing string to paperlist.

--- resources/views/admin/paper/index.blade.php
+++ resources/views/admin/paper/index.blade.php
@@ -50,7 +50,7 @@
                                 {{ gmstrftime('%H:%M:%S',intval($paper->updated_at) - intval($paper->created_at)) }}
                             </td>
 
-                            <td>{{ date('Y-m-d H:i:s', $paper->updated_at) }}</td>
+                            <td>{{ $paper->updated_at==0?"答题中":date('Y-m-d H:i:s', $paper->updated_at) }}</td>
                             <td>
                                 <a href="{{ url('admin/paper') . '/' . $paper->paper_id }}">题目列表</a>
                                 <a href="{{ url('admin/paper') . '/' . $paper->paper_id .'/export'}}" target="_blank">导出EXCEL</a>
@@ -77,28 +77,31 @@
     <!--搜索结果页面 列表 结束-->
 
     <script>
-        function delQuest(question_id) {
-            layer.confirm('您确定要删除此题吗？', {
-                btn: ['确定','取消'] //按钮
-            }, function(){
+      function delQuest(question_id) {
+        layer.confirm('您确定要删除此题吗？', {
+          btn: ['确定', '取消'] //按钮
+        }, function () {
 
-                alert('暂时不允许删除!');return;
-                $.post("{{url('admin/papers/')}}/"+question_id+"/delete",{'_method':'delete','_token':"{{csrf_token()
-                }}"},
-                        function
-                                (data) {
-                            if(data.status==0){
-                                location.href = location.href;
-                                layer.msg(data.msg, {icon: 6});
-                            }else{
-                                layer.msg(data.msg, {icon: 5});
-                            }
-                        });
-//            layer.msg('的确很重要', {icon: 1});
-            }, function(){
-
+          alert('暂时不允许删除!');
+          return;
+          $.post("{{url('admin/papers/')}}/" + question_id + "/delete", {
+              '_method': 'delete', '_token': "{{csrf_token()
+                }}"
+            },
+            function
+              (data) {
+              if (data.status == 0) {
+                location.href = location.href;
+                layer.msg(data.msg, {icon: 6});
+              } else {
+                layer.msg(data.msg, {icon: 5});
+              }
             });
-        }
+//            layer.msg('的确很重要', {icon: 1});
+        }, function () {
+
+        });
+      }
     </script>
 
 @endsection
